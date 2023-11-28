c1da473b2e53ef8a96940170a5eb5400403ddbca
ERFAN pournasiri <erfanpournasiri71@gmail.com>
2023-11-29 00:45:36 +0330

    add router for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import Home from './pages/Home'
 
 export default function App() {
   return (
-    <div className='bg-slate-600'>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/Profile' element={<Profile/>}/>
+    </Routes>
   )
 }
 
