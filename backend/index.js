b454389589a57ea999794e3d8ecfbe646e8a8b09
ERFAN pournasiri <erfanpournasiri71@gmail.com>
2023-11-29 08:04:02 +0330

    create connection for connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
  import express from 'express';
+ import mongoose from 'mongoose';
+ import dotenv from "dotenv"
+
+
+
+dotenv.config()
+
+ mongoose.connect(process.env.MONGO_URL)
+    .then(()=>console.log("mongoDB Connected"))
+    .catch((error)=>console.log(error.message))
 
  const app = express();
 
  app.listen(3000, ()=>{
-    console.log("Server Listening on port 3000")
+    console.log("Server Listening on port 3000 ")
  })
+ 
