1eb79daf4e785231a986b83f9e546d6513b87352
ERFAN pournasiri <erfanpournasiri71@gmail.com>
2023-12-15 10:22:45 +0330

    create delete route in back-end

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { test, updateUser } from '../controllers/user.controller.js'
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js'
 import { verifyToken } from '../util/verifiyUser.js'
 
 const router = express.Router()
 
 router.get('/',test)
 router.post("/update/:id",verifyToken,updateUser)
+router.post("/delete/:id",verifyToken,deleteUserUser)
 
 export default router
